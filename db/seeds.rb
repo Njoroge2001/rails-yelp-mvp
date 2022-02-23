1048598929d23245989eec431982ca63cda5e261
njoroge kimani <therealkenyan@gmail.com>
2022-02-23 15:12:25 +1100

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Wiping seeds"
+Restaurant.destroy_all
+
+puts "Creating seeds"
+
+Restaurant.create(name: "Hoxton 100", address: "Hoxton Street", phone_number: "01234 578990", category: "chinese")
+Restaurant.create(name: "Hoxton 200", address: "Hoxton TwoStreet", phone_number: "01234 840200", category: "italian")
+Restaurant.create(name: "Hoxton 300", address: "Hoxton ThreeStreet", phone_number: "01234 283648", category: "japanese")
+Restaurant.create(name: "Hoxton 400", address: "Hoxton FourStreet", phone_number: "01234 627394", category: "french")
+Restaurant.create(name: "Hoxton 500", address: "Hoxton FiveStreet", phone_number: "01234 987123", category: "belgian")
+
+puts "Seeds created!"
